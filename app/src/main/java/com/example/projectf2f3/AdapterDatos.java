c9eafd707a6acde7455f3527f558f0cd68ab8b3d
Muhammad Hamza <84866952+Hamza2Dam@users.noreply.github.com>
2022-05-24 05:47:25 +0200

    picasso

--- app/src/main/java/com/example/projectf2f3/AdapterDatos.java
+++ app/src/main/java/com/example/projectf2f3/AdapterDatos.java
@@ -3,7 +3,9 @@ package com.example.projectf2f3;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.squareup.picasso.Picasso;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -30,7 +32,12 @@ public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDa
         holder.ElNombre.setText(listaMenu.get(position).getNombre());
         holder.LaDescripcion.setText(listaMenu.get(position).getDescripcion());
         holder.ElPrecio.setText(listaMenu.get(position).getPrecio());
-        holder.LAFoto.setText(listaMenu.get(position).getDescripcion());
+
+        String urla =listaMenu.get(position).getFoto();
+        String urlb ="https://www.google.com/search?q=pizza&rlz=1C1GCEA_enES929ES929&sxsrf=ALiCzsZi4cnXgkt6LF8wP4-vLH6MIwo75g:1653363442879&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi43snlmvf3AhWDyoUKHU62CFkQ_AUoAnoECAMQBA&biw=1920&bih=937&dpr=1#imgrc=OHCj3BQ07nlg4M";
+
+        //Picasso.with(this).load(urla).placeholder(R.mipmap.ic_launcher).into(holder.mAnimeImageView);
+        //Picasso.with(this).load(urlb).into(holder.mAnimeImageView);
 
     }
 
@@ -41,14 +48,15 @@ public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDa
 
     public class ViewHolderDatos extends RecyclerView.ViewHolder {
 
-        TextView ElNombre, LaDescripcion, ElPrecio, LAFoto;
+        TextView ElNombre, LaDescripcion, ElPrecio;
+        ImageView mAnimeImageView;
 
         public ViewHolderDatos( View itemView) {
             super(itemView);
             ElNombre = (TextView) itemView.findViewById(R.id.idTitulo);
             LaDescripcion = (TextView) itemView.findViewById(R.id.idDescipcion);
             ElPrecio = (TextView) itemView.findViewById(R.id.idPrecio);
-            LAFoto = (TextView) itemView.findViewById(R.id.idfoto);
+            mAnimeImageView = (ImageView) itemView.findViewById(R.id.idImagen);
 
         }
     }
